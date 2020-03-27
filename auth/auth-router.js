3fa4fce57e98f3957913260f39bb1af271067f6b
cedric-winbush <cawinbushjr@gmail.com>
2020-03-27 10:34:59 -0500

    create User Model -- create Post call for Login and Register

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,65 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { jwtSecret } = require("../config/secrets.js");
 
-router.post('/register', (req, res) => {
+const Users = require("../users/users-model.js");
+
+router.post("/register", (req, res) => {
   // implement registration
+  const user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  Users.add(user)
+    .then(savedUser => {
+      res.status(201).json(savedUser);
+    })
+    .catch(error => {
+      res.status(500).json({ errorMessage: "Problem registering user" });
+    });
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   // implement login
+  const { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(users => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        console.log(jwtSecret);
+        const token = generateToken(user);
+        console.log(token);
+
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+          token
+        });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
+
+function generateToken(user){
+  const payload ={
+    subject: user.id,
+    username: user.username,
+
+  };
+
+
+  const options = {
+    expiresIn: '1h',
+
+
+  };
+  console.log(payload)
+  return jwt.sign(payload, jwtSecret, options)
+}
 
 module.exports = router;
